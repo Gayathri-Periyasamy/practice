0244b0762606bef2d05d658c46f5c5a69a8236ba
Gayathri Periyasamy <gayathriperiyasamy027@gmail.com>
2025-09-05 15:28:05 +0530

    Time: 0 ms (100%), Space: 44 MB (83.23%) - LeetHub

---
+++ 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        int countNegative = 0;
+        for (int num : nums) {
+            if (num == 0) {
+                return 0;
+            } else if (num < 0) {
+                countNegative++;
+            }
+        }
+        return (countNegative % 2 == 0) ? 1 : -1;
+    }
+}
