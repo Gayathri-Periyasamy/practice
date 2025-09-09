05c4131b20b5e38c65bda2941ba00c0f4788b98e
Gayathri Periyasamy <gayathriperiyasamy027@gmail.com>
2025-09-09 12:09:22 +0530

    Time: 3 ms (86.32%), Space: 44 MB (56%) - LeetHub

---
+++ 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int mostWordsFound(String[] sen) {
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<sen.length;i++){
+            String[] arr=sen[i].split(" ");
+            if(arr.length>max){
+                max=arr.length;
+            }
+        }
+        return max;
+    }
+}
