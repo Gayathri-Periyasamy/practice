918e6b48cbcf179fbd06b6b36726f625ad4f5fdf
Gayathri Periyasamy <gayathriperiyasamy027@gmail.com>
2026-01-29 12:19:29 +0530

    Time: 0 ms (100%), Space: 42.2 MB (90.09%) - LeetHub

---
+++ 0492-construct-the-rectangle/0492-construct-the-rectangle.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] constructRectangle(int area) {
+        int wid=(int)Math.sqrt(area);
+        while(area%wid!=0){
+            wid--;
+        }
+        int len=area/wid;
+        return new int[] {len,wid};
+    }
+}
