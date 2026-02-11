c106f983da27e3d842e39438447f8196734a8768
Gayathri Periyasamy <gayathriperiyasamy027@gmail.com>
2026-02-11 10:02:04 +0530

    Time: 8 ms (14.02%), Space: 48.8 MB (28.67%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] countBits(int n) {
+     int[] ans=new int[n+1];
+     for(int i=0;i<=n;i++){
+        int count=0;
+        for(int j=0;j<32;j++){
+            if(((i>>j)&1)==1)
+            count++;
+        }
+        ans[i]=count;
+     }   
+     return ans;
+    }
+}
